9ac899b3f0f4196b660b5d90ddb788c3c7feb49e
EldarKabarxx <eldar200715@gmail.com>
2025-04-07 14:13:27 +0300

    SCHU-150 email service tests

---
+++ src/test/java/com/techstud/schedule_university/auth/EmailServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.techstud.schedule_university.auth;
+
+import com.techstud.schedule_university.auth.service.impl.EmailServiceImpl;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailServiceImplTest {
+
+    @Mock
+    private JavaMailSender mailSender;
+
+    @InjectMocks
+    private EmailServiceImpl emailService;
+
+    @Test
+    void sendCode_ShouldSendEmailWithCorrectParameters() throws Exception {
+        // Arrange
+        String sender = "no-reply@example.com";
+        String recipient = "user@example.com";
+        String code = "654321";
+
+        ReflectionTestUtils.setField(emailService, "sender", sender);
+        MimeMessage mockMessage = mock(MimeMessage.class);
+        when(mailSender.createMimeMessage()).thenReturn(mockMessage);
+
+        // Act
+        emailService.sendCode(recipient, code);
+
+        // Assert
+        ArgumentCaptor<MimeMessage> messageCaptor = ArgumentCaptor.forClass(MimeMessage.class);
+        verify(mailSender).send(messageCaptor.capture());
+        verify(mailSender).createMimeMessage();
+    }
+
+}
